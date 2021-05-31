9c89a8901fea2e522dbd803ca9508ffdea110986
Islam Gönder <imiseay@gmail.com>
2021-05-31 13:07:58 +0100

    Completed Grid

--- src/components/Contact/ContactElements.jsx
+++ src/components/Contact/ContactElements.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const ContactContainer = styled.div`
-
 min-height: 50vh;
   padding: 5rem calc((100vw - 1300px) / 2);
   background: #150f0f;
@@ -10,32 +9,79 @@ min-height: 50vh;
 
 
 export const ContactWrapper = styled.div`
-
 display: flex;
+flex-wrap: wrap;
+width: 100%;
 justify-content: center;
 align-items: center; 
-
 `;
 
 export const ContactGrid = styled.div`
-
 display: grid;
-grid-template-columns: 20% 20% 20%;
-grid-template-areas: "a b c"
+width: 80%;
+grid-template-columns: 10% 40% 40% 10%;
+grid-template-areas: "a b c d";
+gap: 20px;
+`;
 
+export const GridWrapper = styled.div`
+display: flex;
+height: 100%;
+justify-content: center;
+align-items: center;
 `;
 
 export const ContactGridB = styled.div`
 grid-area: b;
-
 `;
 
 export const ContactGridA = styled.div`
-
 grid-area: a;
-
 `;
 
 export const ContactGridC = styled.div` 
 grid-area: c;
+`;
+
+export const ContactGridD = styled.div` 
+grid-area: d;
+`;
+
+export const InfoWrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: flex-end;
+`;
+
+export const ContactInfoTitle = styled.h2`
+font-weight: 300;
+`;
+
+export const GridWrapperTitle = styled.div`
+display: flex;
+height: 100%;
+justify-content: flex-end;
+align-items: center;
+`;
+
+export const ContactInfoText = styled.p`
+
+`;
+
+export const InfoGrid = styled.div`
+display: grid;
+
+grid-template-columns: repeat(2, 1fr);
+grid-template-rows: repeat(4, 1fr);
+grid-template-areas: 
+"Address AddressInfo"
+"Telephone TelephoneInfo"
+"Mail MailInfo"
+"Open OpenInfo";
+row-gap: 20px;
+
+`;
+
+export const InfroGridAddress = styled.div`
 `;
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
-import { ContactContainer, ContactWrapper, ContactGrid, ContactGridB } from './ContactElements'
+import { ContactContainer, ContactWrapper, InfoGrid, ContactGrid, ContactInfoText, GridWrapper, GridWrapperTitle, ContactGridA, ContactGridB, ContactGridC, ContactGridD, ContactInfoTitle, InfoWrapper } from './ContactElements'
 
 // import './ContactUs.css';
 
@@ -21,18 +21,38 @@ export default function Contact() {
         <ContactContainer >
             <ContactWrapper>
                 <ContactGrid>
+                    <ContactGridA>
+                        <GridWrapperTitle>
+                            <h1>Contact</h1>
+                        </GridWrapperTitle>
+                    </ContactGridA>
                     <ContactGridB>
-                        <form id="b" className="contact-form" onSubmit={sendEmail}>
-                            <input type="hidden" name="contact_number" />
-                            <label>Name</label>
-                            <input type="text" name="user_name" />
-                            <label>Email</label>
-                            <input type="email" name="user_email" />
-                            <label>Message</label>
-                            <textarea name="message" />
-                            <input type="submit" value="Send" />
-                        </form>
+                        <GridWrapper>
+                            <form className="contact-form" onSubmit={sendEmail}>
+                                <input type="hidden" name="contact_number" />
+                                <label>Name</label>
+                                <br></br>
+                                <input type="text" name="user_name" /><br></br>
+                                <label>Email</label><br></br>
+                                <input type="email" name="user_email" /><br></br>
+                                <label>Message</label><br></br>
+                                <textarea name="message" /><br></br>
+                                <input type="submit" value="Send" />
+                            </form>
+                        </GridWrapper>
                     </ContactGridB>
+                    <ContactGridC>
+                        <InfoGrid>
+                            <ContactInfoTitle>Address</ContactInfoTitle>
+                            <ContactInfoText>Av. 25 de Abril 10, 2825-416 Costa da Caparica</ContactInfoText>
+                            <ContactInfoTitle>Tel.</ContactInfoTitle>
+                            <ContactInfoText>+351 21 290 1695</ContactInfoText>
+                            <ContactInfoTitle>E-Mail</ContactInfoTitle>
+                            <ContactInfoText>a-transmontana@hotmail.com</ContactInfoText>
+                            <ContactInfoTitle>Open</ContactInfoTitle>
+                            <ContactInfoText>Mon-Thu: 11.00-23.00<br></br>Fr-Sun: 11:00-02:00</ContactInfoText>
+                        </InfoGrid>
+                    </ContactGridC>
                 </ContactGrid>
             </ContactWrapper>
         </ContactContainer>
